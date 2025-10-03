100cc4b51419432a7213ee67caf57feb57e4668b
Kabilan <kabilanvennila@gmail.com>
2025-10-03 15:10:44 +0530

    Adding requirements

--- tradeproject/settings.py
+++ tradeproject/settings.py
@@ -61,6 +61,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     "http://127.0.0.1:5175",
+    "https://trader-em8b.onrender.com",
+    "https://trader-em8b.onrender.com:8000"
 ]
 
 CORS_ALLOW_HEADERS = [
