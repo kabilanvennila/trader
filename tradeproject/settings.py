53c0280381c99c919dce9b41b87e030aefbd9b95
Kabilan <kabilanvennila@gmail.com>
2025-10-03 15:42:23 +0530

    Adding hosts

--- tradeproject/settings.py
+++ tradeproject/settings.py
@@ -138,7 +138,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://trader-em8b.onrender.com']
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
